d54ba374b910d057c658264f6491d8276a3f2b6f
vishnu45 <vishnu4v5@gmail.com>
2018-08-01 01:11:54 -0400

    ADD-F: DriverActions - FindElement, Click, GetText

--- WinAuto/src/main/java/labs/vish/automation/core/DriverActions.java
+++ WinAuto/src/main/java/labs/vish/automation/core/DriverActions.java
@@ -1,5 +1,50 @@
 package labs.vish.automation.core;
 
+import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.WebElement;
+
 public class DriverActions {
     
+    /**
+     * To get the corresponding WebElement object of the element locator provided
+     */
+    public static WebElement FindElement(Locator locator) {
+        WebElement element = null;
+        
+        if (locator.getValue().contains("automationId=")) {
+            element = TestBase.AppSession
+                .findElementByAccessibilityId(StringUtils.substringAfter(locator.getValue(), "automationId="));
+        }
+        if (locator.getValue().contains("className=")) {
+            element = TestBase.AppSession
+            .findElementByClassName(StringUtils.substringAfter(locator.getValue(), "className="));
+        }
+        if (locator.getValue().contains("name=")) {            
+            element = TestBase.AppSession            
+                .findElementByName(StringUtils.substringAfter(locator.getValue(), "name="));
+        }
+        if (locator.getValue().contains("xpath=")) {
+            element = TestBase.AppSession
+                .findElementByName(StringUtils.substringAfter(locator.getValue(), "xpath="));
+        }
+        
+        return element;    
+    }
+
+    /**
+     * Action to click particular locator element
+     */
+    public static void Click(Locator locator) {        
+        FindElement(locator).click();
+    }
+
+    /**
+     * Action to get embedded text in locator
+     */
+    public static String GetText(Locator locator) {        
+        return FindElement(locator).getText();
+    }
+
+
+
 }
